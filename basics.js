d9b9a9a1545a9c0411605e643e0a10fc67863642
kandasanivyshnavi <kandasanivyshnavi@gmail.com>
2025-04-05 15:03:59 +0530

    Create basics.js
    basic

---
+++ basics.js
@@ -0,0 +1,26 @@
+console.log('vyshnavi');
+let name = "vyshnavi";
+console.log(name);
+//   .....VARIABLES..... CONSTANTS....
+//  let , var ,const ...
+//  always use meningful and descriptive name
+//  cannot start with number
+//  should not be space or -
+//  camle notation to name variables
+//  case sensitive
+//  name1 , name2 ...
+//  best practice is to declare each variable in seprate line
+//  if we dont need to reassign then  use " const " keyword
+
+//  primitive  and reference 
+// ...primitives...
+//   String  , Number , Boolean , undefined , null
+let F_name = "vyshnavi_kandasani";
+let age = 20;
+let state = true;
+let  lName = undefined;
+let mNme = null;
+console.log(F_name);
+console.log(age);
+console.log(state);
+console.log(lName);
