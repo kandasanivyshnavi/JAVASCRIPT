1b00079f9c612395d9b3516dea745b0b0a3b8529
kandasanivyshnavi <kandasanivyshnavi@gmail.com>
2025-04-05 15:14:21 +0530

    Array.js
    Arrays methods

---
+++ Array.js
@@ -0,0 +1,42 @@
+
+// .................ARRAYS ..........
+
+// new is a keyword ,it is  a constructer
+// const numbers = new Array(1,2,3);
+/*methods of arrays 
+-        .push(literal);
+-        .pop();
+-        .unshift() ; -- to add a value in the begging 
+-        .indexOf() ;  -- to get the index of a value 
+
+*/
+
+const number = new Array(1,2,3);
+
+console.log(number);
+
+const numbers = [1,2,3,4,5];
+
+console.log(numbers);
+
+console.log(numbers.push(10));
+
+
+console.log(numbers);
+
+console.numbers.push(10);
+
+numbers.unshift(10);
+
+console.log(numbers);
+
+numbers.pop(10);
+
+console.log(numbers.indexOf(10));
+
+console.log(numbers.indexOf(10));
+
+numbers.push(10);
+
+console.log(numbers.indexOf(10));
+
