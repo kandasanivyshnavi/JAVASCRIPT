8cc4c4e2bcbc9c0ce9bba2d4b1810b0e64001f4c
kandasanivyshnavi <kandasanivyshnavi@gmail.com>
2025-04-06 11:34:46 +0530

    forEach_loop.js
    forEach loop

---
+++ forEach.js
@@ -0,0 +1,24 @@
+/* 
+the synatx of forEach is 
+
+array_name . forEach( Function(..// iterator eg : i , j)   {../opration ..}  ) ;
+array_name .forEach (function() {} ) ;
+
+ */
+
+let nums = [1,2,3,4,5,6,7];
+
+nums.forEach(function(i) {console.log(i)});
+
+/*
+output :
+
+1
+2
+3
+4
+5
+6
+7
+
+*/
