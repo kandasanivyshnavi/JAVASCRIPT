773aa188d7b88d47cfe5921729c9c8d2870bc100
kandasanivyshnavi <kandasanivyshnavi@gmail.com>
2025-04-06 14:53:30 +0530

    Add files via upload

---
+++ DOM_1.js
@@ -0,0 +1,15 @@
+// // reading the document objects 
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.url);
+// console.log(document.domain);
+// console.log(document.doctype);
+// console.log(document.body)
+
+// // writing the document objects
+
+// document.title = 'MAMATHA';
+// // console.dir(document);
+// console.log(document.title);
+
+console.log(document.getElementById('header-title'));
